40e9b17f0188508ccbc051dc404fbac9af61b406
phu <quyvo2079@gmail.com>
2026-06-03 11:39:46 +0700

    cau hinh cors cho FE

--- TAWatch /TAWatch/src/main/java/TAWactch/example/TAWatch/security/SecurityConfig.java
+++ TAWatch /TAWatch/src/main/java/TAWactch/example/TAWatch/security/SecurityConfig.java
@@ -42,7 +42,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://dongho-tawatch.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
