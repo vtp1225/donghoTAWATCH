4dda027aaa10a82bdd001ff8e567316f39b608e5
phu <quyvo2079@gmail.com>
2026-06-24 12:13:17 +0700

    chinh sua Fe

--- TAWATCHFE/tawatch/vite.config.js
+++ TAWATCHFE/tawatch/vite.config.js
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/recharts') || id.includes('node_modules/d3')) {
+            return 'vendor-charts'
+          }
+          if (id.includes('node_modules/react-router-dom') || id.includes('node_modules/react-router/') || id.includes('node_modules/@remix-run')) {
+            return 'vendor-router'
+          }
+          if (id.includes('node_modules/react/') || id.includes('node_modules/react-dom/') || id.includes('node_modules/scheduler/')) {
+            return 'vendor-react'
+          }
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       '/tawatch': {
